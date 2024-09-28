f78bd1876f5da8dd3df6c624455f41537eb803e7
Legamer-exe <charles-adrien.merle@coda-student.school>
2024-09-28 19:26:12 +0200

    etape 4 : créer les conditions

--- projet/main.c
+++ projet/main.c
@@ -6,18 +6,33 @@ int main() {
     int num2;
     char operateur;
 
-    printf("choisissez un opérateur parmi les suivants (+, -, *, /, %%): ");
+    printf("Choisissez un opérateur parmi les suivants (+, -, *, /, %%): ");
     scanf(" %c", &operateur);
 
-    printf("entrez le premier nombre entier: ");
+
+    printf("Entrez le premier nombre entier: ");
     scanf("%d", &num1);
 
-    printf("entrez le deuxième nombre entier: ");
+    printf("Entrez le deuxième nombre entier: ");
     scanf("%d", &num2);
 
-    printf("num_1: %d\n", nombre1);
-    printf("num_2: %d\n", nombre2);
+    printf("num_1: %d\n", num1);
+    printf("num_2: %d\n", num2);
     printf("operation: %c\n", operateur);
+
+    if (operateur == '+') {
+        printf("addition\n");
+    } else if (operateur == '-') {
+        printf("soustraction\n");
+    } else if (operateur == '*') {
+        printf("multiplication\n");
+    } else if (operateur == '/') {
+        printf("division\n");
+    } else if (operateur == '%') {
+        printf("modulo\n");
+    } else {
+        printf("L'opérateur n'est pas reconnu\n");
+    }
 
     return 0;
 }
