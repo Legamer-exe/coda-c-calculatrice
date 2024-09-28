ce09351ab19c48a99a2aeb4d5e47134aea08a78c
Legamer-exe <charles-adrien.merle@coda-student.school>
2024-09-28 19:57:34 +0200

    etape 5 : l'opération

--- projet/main.c
+++ projet/main.c
@@ -5,10 +5,12 @@ int main() {
     int num1;
     int num2;
     char operateur;
+    int result;
+    float resultat;
+    int resultmodulo;
 
     printf("Choisissez un opérateur parmi les suivants (+, -, *, /, %%): ");
     scanf(" %c", &operateur);
-
 
     printf("Entrez le premier nombre entier: ");
     scanf("%d", &num1);
@@ -21,15 +23,33 @@ int main() {
     printf("operation: %c\n", operateur);
 
     if (operateur == '+') {
+        result = num1 + num2;
         printf("addition\n");
+        printf("résultat: %d\n", result);
     } else if (operateur == '-') {
+        result = num1 - num2;
         printf("soustraction\n");
+        printf("résultat: %d\n", result);
     } else if (operateur == '*') {
+        result = num1 * num2;
         printf("multiplication\n");
+        printf("résultat: %d\n", result);
     } else if (operateur == '/') {
-        printf("division\n");
+        if (num2 != 0) {
+            resultat = (float)num1 / num2;
+            printf("division\n");
+            printf("résultat: %f\n", resultat);
+        } else {
+            printf("Erreur : il est impossible de diviser par zéro !\n");
+        }
     } else if (operateur == '%') {
-        printf("modulo\n");
+        if (num2 != 0) {
+            resultmodulo = num1 % num2;
+            printf("modulo\n");
+            printf("résultat: %d\n", resultmodulo);
+        } else {
+            printf("Erreur : il est impossible de diviser par zéro pour le modulo !\n");
+        }
     } else {
         printf("L'opérateur n'est pas reconnu\n");
     }
