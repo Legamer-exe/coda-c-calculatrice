74ad18d29751aee5f0dcae67f7451ec0ad0ffd7f
Legamer-exe <charles-adrien.merle@coda-student.school>
2024-09-28 19:20:50 +0200

    etape 3 : récupérer les infos

--- projet/main.c
+++ projet/main.c
@@ -2,5 +2,22 @@
 #include <stdio.h>
 
 int main() {
+    int num1;
+    int num2;
+    char operateur;
+
+    printf("choisissez un opérateur parmi les suivants (+, -, *, /, %%): ");
+    scanf(" %c", &operateur);
+
+    printf("entrez le premier nombre entier: ");
+    scanf("%d", &num1);
+
+    printf("entrez le deuxième nombre entier: ");
+    scanf("%d", &num2);
+
+    printf("num_1: %d\n", nombre1);
+    printf("num_2: %d\n", nombre2);
+    printf("operation: %c\n", operateur);
+
     return 0;
 }
